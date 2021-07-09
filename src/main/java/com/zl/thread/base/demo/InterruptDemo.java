932d42f08b5338efb755821756ea25a862db24f4
Sunny ZL <56599784+SunnyZ-L@users.noreply.github.com>
2021-07-09 16:00:44 +0800

    Update InterruptDemo.java

--- src/main/java/com/zl/thread/base/demo/InterruptDemo.java
+++ src/main/java/com/zl/thread/base/demo/InterruptDemo.java
@@ -12,8 +12,8 @@ public class InterruptDemo {
      * java.lang.Thread#interrupt()方法并不会终止一个线程
      *
      *
-     * java.lang.Thread#interrupted() 测试 当前 线程是否已经中断，当前线程是指运行this.interrupted方法的线程
-     * java.lang.Thread#isInterrupted() 测试线程是否已经停止中断
+     * java.lang.Thread#interrupted() 静态方法。测试 当前 线程是否已经中断，当前线程是指运行this.interrupted方法的线程
+     * java.lang.Thread#isInterrupted() 非静态方法。测试线程是否已经停止中断
      */
     public static void main(String[] args) {
         try {
